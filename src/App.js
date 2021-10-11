904353af600ccc80a4fdbb388f50e339a0e9e1b7
gsonkow <sonkowskyg@gmail.com>
2021-10-11 11:45:33 -0700

    incorporated todoList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+//import './App.css';
+import { useState } from 'react';
+import TodoList from "./TodoList";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, content: 'Meet Masao for Udon'},
+    {id: 2, content: 'Fix up these horrible React notes'},
+    {id: 3, content: 'End class on time'},
+  ]);
   return (
     <div className="App">
-      <h1>BASIC TODO APP</h1>
+      <h1>Things To Do</h1>
+      <TodoList todos={todos} />
     </div>
   );
 }
